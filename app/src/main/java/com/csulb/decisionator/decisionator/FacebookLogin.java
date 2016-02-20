cec2cd4436a368ba6cb3b6cc59785895fb1ec602
Russell Tan <sell_nat@yahoo.com>
2016-02-20 10:45:33 -0800

    Can now login, get greeting, navigate to event creation

--- app/src/main/java/com/csulb/decisionator/decisionator/FacebookLogin.java
+++ app/src/main/java/com/csulb/decisionator/decisionator/FacebookLogin.java
@@ -2,22 +2,34 @@ package com.csulb.decisionator.decisionator;
 
 
 import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
-
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import org.json.JSONObject;
+
 public class FacebookLogin extends AppCompatActivity {
+
+    //Keys for the intent
+    protected final static String USER_F_NAME = "com.csulb.decisionator.USER_F_NAME";
+    protected final static String USER_ID = "com.csulb.decisionator.USER_ID";
+    protected final static String USER_AUTH = "com.csulb.decisionator.USER_AUTH";
 
     private CallbackManager callbackManager;
     private LoginButton loginButton;
     private TextView info;
+    private Intent loginSuccess;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,16 +40,45 @@ public class FacebookLogin extends AppCompatActivity {
         setContentView(R.layout.activity_facebook_login);
         info = (TextView)findViewById(R.id.info);
         loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginSuccess = new Intent(this, LobbyActivity.class);
+
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult){
-                info.setText(
-                        "User ID: "
-                                + loginResult.getAccessToken().getUserId()
-                                + "\n" +
-                                "Auth Token: "
-                                + loginResult.getAccessToken().getToken()
-                );
+            /*
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                try {
+                                    String name = object.getString("name");
+                                    String email = object.getString("email");
+                                    String birthday = object.getString("birthday"); // 01/31/1980 format
+                                }
+                                catch (Exception e)
+                                {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender, birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
+
+            */
+                Profile me = Profile.getCurrentProfile();
+                me.getFirstName();
+
+
+
+                //JSONObject profile = Util.parseJson(facebook.request("me"));
+                loginSuccess.putExtra(USER_F_NAME, me.getFirstName());
+                loginSuccess.putExtra(USER_ID, loginResult.getAccessToken().getUserId());
+                loginSuccess.putExtra(USER_AUTH, loginResult.getAccessToken());
+
+                startActivity(loginSuccess);
             }
             @Override
             public void onCancel() {
