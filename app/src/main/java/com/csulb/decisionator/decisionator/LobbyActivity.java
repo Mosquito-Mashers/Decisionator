866867680672069ea871e75500e577e8b2211f07
sellnat77 <sell_nat@yahoo.com>
2016-02-25 22:51:58 -0800

    Lobby activity cleaned up

--- app/src/main/java/com/csulb/decisionator/decisionator/LobbyActivity.java
+++ app/src/main/java/com/csulb/decisionator/decisionator/LobbyActivity.java
@@ -7,40 +7,77 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Iterator;
+import java.util.Map;
+
 public class LobbyActivity extends AppCompatActivity {
 
+    //Values retrieved from intent
+    String uName;
+    String uID;
+    String welcomeString;
+    String poolID;
+    Map<String, String> intentPairs = null;
+
+    //Gui items
     private Intent loginSuccess;
     private Intent createEventIntent;
-
     private TextView welcomeMessage;
     private Button createEvent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        initializeGlobals();
+
+        initializeListeners();
+
+        prepareIntent(createEventIntent, intentPairs);
+
         setContentView(R.layout.activity_lobby);
+    }
 
-        loginSuccess = getIntent();
+    private void prepareIntent(Intent createEventIntent, Map<String, String> intentPairs) {
+        Iterator mapIter = intentPairs.entrySet().iterator();
 
-        welcomeMessage = (TextView) findViewById(R.id.welcomeText);
-        createEvent = (Button) findViewById(R.id.createEvent);
-        createEventIntent = new Intent(this, EventCreationActivity.class);
+        while (mapIter.hasNext())
+        {
+            Map.Entry kvPair = (Map.Entry) mapIter.next();
+            createEventIntent.putExtra(kvPair.getKey().toString(), kvPair.getValue().toString());
+        }
+    }
 
-        String uName = loginSuccess.getStringExtra(FacebookLogin.USER_F_NAME);
-        String uID = loginSuccess.getStringExtra(FacebookLogin.USER_ID);
-        String welcomeString = welcomeMessage.getText() + " " + uName + "!";
-        String poolID = loginSuccess.getStringExtra(FacebookLogin.POOL_ID);
-        createEventIntent.putExtra(FacebookLogin.POOL_ID, poolID);
-        createEventIntent.putExtra(FacebookLogin.USER_ID, uID);
-        createEventIntent.putExtra(FacebookLogin.USER_F_NAME,uName);
-
-        welcomeMessage.setText(welcomeString);
-
+    private void initializeListeners() {
         createEvent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(createEventIntent);
             }
         });
+    }
+
+    private void initializeGlobals() {
+        //Intent Initialization
+        loginSuccess = getIntent();
+        createEventIntent = new Intent(this, EventCreationActivity.class);
+
+        //GUI assignments
+        welcomeMessage = (TextView) findViewById(R.id.welcomeText);
+        createEvent = (Button) findViewById(R.id.createEvent);
+
+        //Prepare outgoing intent
+        intentPairs.put(FacebookLogin.POOL_ID, poolID);
+        intentPairs.put(FacebookLogin.USER_ID, uID);
+        intentPairs.put(FacebookLogin.USER_F_NAME, uName);
+
+        //Global string values
+        uName = loginSuccess.getStringExtra(FacebookLogin.USER_F_NAME);
+        uID = loginSuccess.getStringExtra(FacebookLogin.USER_ID);
+        poolID = loginSuccess.getStringExtra(FacebookLogin.POOL_ID);
+
+        //GUI Update based on intent
+        welcomeString = welcomeMessage.getText() + " " + uName + "!";
+        welcomeMessage.setText(welcomeString);
     }
 }
