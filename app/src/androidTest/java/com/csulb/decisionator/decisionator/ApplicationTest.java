7ea8e2aa8d145517b9f15ad2f29f914b8c128495
jtjr <enlightenedsamurai@gmail.com>
2016-03-17 21:14:02 -0700

    aws working need to add unath permission

--- app/src/androidTest/java/com/csulb/decisionator/decisionator/ApplicationTest.java
+++ app/src/androidTest/java/com/csulb/decisionator/decisionator/ApplicationTest.java
@@ -1,13 +1,46 @@
 package com.csulb.decisionator.decisionator;
 
 import android.app.Application;
+import android.content.Context;
 import android.test.ApplicationTestCase;
+
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
+
+
     public ApplicationTest() {
         super(Application.class);
     }
+
+    @Override
+    public void setUp() throws Exception {
+        CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
+                getContext(),   /* get the context for the application */
+                "us-east-1:a74e3f8c-6c2b-40b6-89d5-46d4f870a6f2", // Identity Pool ID
+                Regions.US_EAST_1           /* Region for your identity pool--US_EAST_1 or EU_WEST_1*/
+        );
+        AmazonDynamoDBClient ddbClient = new AmazonDynamoDBClient(credentialsProvider);
+        DynamoDBMapper mapper = new DynamoDBMapper(ddbClient);
+        User temp = mapper.load(User.class,"russell-2345");
+        String check = temp.getUserID();
+        String check1 = temp.getfName();
+        String check1_1 = "Russ";
+        User temp1 = null;
+        temp1.setUserID("russell-2345");
+
+        assertEquals(check,temp1);
+    }
+
+
 }
