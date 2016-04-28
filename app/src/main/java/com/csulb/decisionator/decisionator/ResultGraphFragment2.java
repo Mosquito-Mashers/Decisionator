1d0c91ee197ff480e3206f860eb00fb3fd41f109
Ronald <ronald41193@gmail.com>
2016-04-27 21:29:28 -0700

    Saving progress before mapping UI to fragment#2

--- app/src/main/java/com/csulb/decisionator/decisionator/ResultGraphFragment2.java
+++ app/src/main/java/com/csulb/decisionator/decisionator/ResultGraphFragment2.java
@@ -7,10 +7,13 @@ import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,10 +24,13 @@ import java.util.Set;
 public class ResultGraphFragment2 extends Fragment {
 
     private String data_for_cloud = "";
-    private String sorted_venues = "";
+    private String top_venues = "";
     private SpannableString wordCloud;
     WordCloudGenerator cloudGen;
     private Map<String,Integer> sortedResults = new HashMap<String, Integer>();
+
+    private TextView box;
+
 
     public static ResultGraphFragment newInstance(Bundle b) {
         ResultGraphFragment fragment = new ResultGraphFragment();
@@ -37,26 +43,30 @@ public class ResultGraphFragment2 extends Fragment {
                 Bundle savedInstanceState) {
             View view = inflater.inflate(R.layout.fragment_result_graph, container, false);
             Bundle incoming = getArguments();
-            TextView box = (TextView) view.findViewById(R.id.venues_description);
-            //TextView description = (TextView) view.findViewById(R.id.venues_cloud);
+
+            box = (TextView) view.findViewById(R.id.venues_description);
+            //TextView sortedData
+           // ListView description = (ListView) view.findViewById(R.id.venues_list);
             box.setMovementMethod(new ScrollingMovementMethod());
-            if (incoming != null) {
-                sorted_venues = getArguments().getString(EventActivity.TOP_VENUE_DATA);
-                if (sorted_venues != "" && sorted_venues != null) {
-                    String raw[] = sorted_venues.split("\\|");
+            if (incoming != null)
+                {
+                data_for_cloud = getArguments().getString(EventActivity.WORD_CLOUD_DATA);
+                top_venues = getArguments().getString(EventActivity.TOP_VENUE_DATA);
+                cloudGen = new WordCloudGenerator(top_venues,null);
+
+                if (top_venues != "" && top_venues != null)
+                {
+                    String raw[] = top_venues.split("\\|");
                     for (int k = 0; k < raw.length; k++) {
                         String item[] = raw[k].split(",");
                         sortedResults.put(item[0], Integer.parseInt(item[1]));
                     }
-
                 }
 
-                box.setText(wordCloud);
-                return view;
+                box.setText(cloudGen.buildSmallMap());
             }
             return view;
         }
-
 
 }
 
